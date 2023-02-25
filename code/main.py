dd3752cdc0e7e64e499f5e62cbbdafdd5663e5e1
Alexander Carvalho <lagercarvalho@gmail.com>
2023-02-25 23:55:01 +0100

    started working of design

--- code/main.py
+++ code/main.py
@@ -1,10 +1,10 @@
 import sys, os
+from pathlib import Path
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QFileDialog, QAbstractItemView, QSizePolicy
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtGui import QIcon
-import comtypes
-
+#import comtypes
 
 class ListboxWidget(QListWidget):
     def __init__(self, parent=None):
@@ -12,6 +12,7 @@ class ListboxWidget(QListWidget):
         self.setAcceptDrops(True)
         self.setGeometry(20, 70, 400, 400)
         self.supportedTypes = (".pdf", ".pptx", ".odp",".doc",".docx",".odt")
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls:
@@ -38,11 +39,24 @@ class ListboxWidget(QListWidget):
                     urlName = str(url.toLocalFile())
                     if (urlName.endswith(self.supportedTypes)):
                         links.append(urlName)
-
+                else:
+                    urlName = str(url.toString())
+                    if (urlName.endswith(self.supportedTypes)):
+                        links.append(urlName)
             self.addItems(links)
         else:
             event.ignore()
 
+
+class Button(QPushButton):
+    def __init__(self, name = "None", parent=None):
+        super().__init__(name, parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+class Label(QtWidgets.QLabel):
+    def __init__(self,parent=None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
 
 class AppWindow(QMainWindow):
@@ -52,38 +66,62 @@ class AppWindow(QMainWindow):
         self.setWindowTitle("Pdf Merger")
         self.setWindowIcon(QIcon("img/pdf_icon.png"))
 
-        self.label = QtWidgets.QLabel(self)
+        self.label = Label(self)
         self.label.setText("Add the files to convert and merge:")
         self.label.setGeometry(20, 20, 200, 20)
+        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         self.lstView = ListboxWidget(self)
 
-        self.mergeBtn = QPushButton("Merge", self)
-        self.mergeBtn.setGeometry(485, 375, 150, 50)
+        self.upBtn = Button("Up", self)
+        self.upBtn.setGeometry(430, 85, 50, 50)
+        self.upBtn.clicked.connect(self.moveUp)
+
+        self.downBtn = Button("Down", self)
+        self.downBtn.setGeometry(430, 405, 50, 50)
+        self.downBtn.clicked.connect(self.moveDown)
+
+        self.mergeBtn = Button("Merge", self)
+        self.mergeBtn.setGeometry(500, 305, 150, 50)
         self.mergeBtn.clicked.connect(self.merge)
 
-        self.browseBtn = QPushButton("Browse", self)
-        self.browseBtn.setGeometry(485, 275, 150, 50)
+        self.browseBtn = Button("Browse", self)
+        self.browseBtn.setGeometry(500, 205, 150, 50)
         self.browseBtn.clicked.connect(self.searchFile)
+        
+        self.show()
 
     def merge(self):
         for item in self.lstView.items():
             #todo continue working on the merge function
-
+            break
 
     def searchFile(self):
         permissions = "PDF files (*.pdf);;Powerpoint files (*.pptx, *.odp);;Word documents(*.doc,*.docx,*odt)"
         fileName = QFileDialog.getOpenFileName(self, 'Open File', os.environ['HOMEPATH'], permissions)
-        if not fileName:
-            self.lstView.addItems(fileName[0])
+        print(fileName)
+        if fileName:
+            self.lstView.addItem(fileName[0])
+
+    def move(self, dir):
+        currentRow = self.currentRow()
+        currentItem = self.takeItem(currentRow)
+        self.insertItem(currentRow + dir, currentItem)
+
+    def moveUp(self):
+        self.move(self, -1)
+
+    def moveDown(self):
+        self.move(self, 1)
 
 
-def window():
+def main():
     app = QApplication(sys.argv)
+    app.setStyleSheet(Path('style.qss').read_text())
 
     win = AppWindow()
-    win.show()
 
     sys.exit(app.exec_())
 
-window()
+if __name__ == "__main__":
+    main()
